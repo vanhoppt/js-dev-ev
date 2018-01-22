2216df3bc7c45f87368eec0acf9758c8c780cc95
Le Van Hop <av.votinh@gmail.com>
2018-01-22 18:10:42 +0700

    add webpack and config webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+import path from 'path';
+
+export default {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(js\jsx)$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
