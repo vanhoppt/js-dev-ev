2a724e555f2fca445e733c7044d22fdf9ad41e3c
Le Van Hop <av.votinh@gmail.com>
2018-01-23 19:40:18 +0700

    Add HTTP Calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
-import './index.css';
-import numeral from 'numeral';
+import {getUsers, deleteUsers} from './api/userApi';
 
-/* eslint-disable no-console */
+getUsers().then(result => {
+  let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log('I would pay ' + courseValue +'for this awesome course!');
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array  from a DOM collection
+  // getElementsByClassName only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUsers(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  }); 
+});
